34736072a202a4bfda5e05294cd29af9a0c59ed3
elenabaurkot <elenabaurkot@gmail.com>
2020-10-11 00:19:41 -0500

    vanilla javascript updated to jquery

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-window.onscroll = stickyNav;
+$(document).ready(function () {
+  // On window scroll, call function stickyNav
+  $(window).scroll(stickyNav);
+  // sets variable to measure distance from navbar to top of screen
+  var distanceToTop = navbar.offsetTop;
 
-var distanceToTop = navbar.offsetTop;
-
-function stickyNav() {
-  const nav = document.querySelector('#navbar');
-
-  if (window.pageYOffset >= distanceToTop) {
-    nav.classList.add('sticky');
-  } else if (window.pageYOffset <= distanceToTop) {
-    nav.classList.remove('sticky');
+  function stickyNav() {
+    const navEl = $('#navbar');
+    // add the sticky class to the navbar if user scrolls past the distance from the top to the navbar
+    if (window.pageYOffset >= distanceToTop) {
+      navEl.addClass('sticky');
+      // if they scroll back up remove the sticky class so the navbar goes back to regular positioning
+    } else if (window.pageYOffset <= distanceToTop) {
+      navEl.removeClass('sticky');
+    }
   }
-}
+});
+
+
+
+
+//  VANILLA JAVASCRIPT
+// window.onscroll = stickyNav;
+
+// var distanceToTop = navbar.offsetTop;
+
+// function stickyNav() {
+//   const navEl = document.querySelector('#navbar');
+
+//   if (window.pageYOffset >= distanceToTop) {
+//     navEl.classList.add('sticky');
+//   } else if (window.pageYOffset <= distanceToTop) {
+//     navEl.classList.remove('sticky');
+//   }
+// }
