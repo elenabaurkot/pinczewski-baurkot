6e4001177c5ea34be2c83d4a2924626a0be39a7f
elenabaurkot <elenabaurkot@gmail.com>
2020-10-10 23:57:18 -0500

    navbar only sticky after scrolling past where it should begin being sticky

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.onscroll = stickyNav;
 
+var distanceToTop = navbar.offsetTop;
+
 function stickyNav() {
   const nav = document.querySelector('#navbar');
-  nav.classList.add('sticky');
+
+  if (window.pageYOffset >= distanceToTop) {
+    nav.classList.add('sticky');
+  } else if (window.pageYOffset <= distanceToTop) {
+    nav.classList.remove('sticky');
+  }
 }
